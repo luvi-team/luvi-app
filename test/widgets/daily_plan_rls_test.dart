b4e3b3d6d7ec2f8d5734e2fe5be028c0f59910eb
Arwin Farajpoory <251047649+epiclori1020@users.noreply.github.com>
2025-09-07 13:23:41 +0200

    test: fix lints in daily_plan test (unused import, key rule ignore)

--- test/widgets/daily_plan_rls_test.dart
+++ test/widgets/daily_plan_rls_test.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
   group('Daily Plan RLS Widget Tests', () {
