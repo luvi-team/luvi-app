a521774081a62212f3fe22ff486c03f4a5e2363c
Arwin Farajpoory <251047649+epiclori1020@users.noreply.github.com>
2025-09-08 10:17:20 +0200

    chore: align log_consent responses to contract tests

---
+++ lib/consent/consent_service.dart
@@ -0,0 +1,20 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class ConsentService {
+  Future<void> accept({
+    required String version,
+    required List<String> scopes,
+  }) async {
+    final response = await Supabase.instance.client.functions.invoke(
+      'log_consent',
+      body: {
+        'version': version,
+        'scopes': scopes,
+      },
+    );
+
+    if (response.status != 200) {
+      throw Exception('Failed to log consent: ${response.status}');
+    }
+  }
+}
