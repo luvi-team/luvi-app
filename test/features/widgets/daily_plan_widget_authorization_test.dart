f5f0a775cfe80ebc85786204e0fa8d50e43b19e7
epiclori1020 <epiclori1020@gmail.com>
2025-10-28 16:21:01 +0100

    Update test/features/widgets/daily_plan_widget_authorization_test.dart
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- test/features/widgets/daily_plan_widget_authorization_test.dart
+++ test/features/widgets/daily_plan_widget_authorization_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-
 import '../../support/test_config.dart';
 
 class DailyPlan {
