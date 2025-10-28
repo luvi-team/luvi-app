d95079be08514ead9f581bf4b816eae61e1c49ef
Arwin Farajpoory <epiclori1020@gmail.com>
2025-10-28 21:40:04 +0100

    fix(ui/tests): remove const around runtime OnboardingHeader; add Riverpod import for StateProvider in daily_plan test

--- test/features/widgets/daily_plan_widget_authorization_test.dart
+++ test/features/widgets/daily_plan_widget_authorization_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod/legacy.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import '../../support/test_config.dart';
